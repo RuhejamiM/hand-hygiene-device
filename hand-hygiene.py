c0e1bc0240c495559937bbff6133ea5faa9082ac
Ruhejami Mustari <62192188+RuhejamiM@users.noreply.github.com>
2022-03-02 14:12:59 -0500

    Update hand-hygiene.py with stub functions

--- hand-hygiene.py
+++ hand-hygiene.py
@@ -1,1 +1,73 @@
-# TODO:
+# hand-hygiene.py
+
+# define global variables
+global DISPLAY_ADDRESS
+
+# TODO: DISPLAY_ADDRESS = 
+
+class HandHygiene:
+    def __init__(self):
+        self.display = None
+        # TODO
+        return
+
+    def setup_display(self, address = DISPLAY_ADDRESS):
+        # TODO
+        return
+
+    def clear_display(self):
+        # TODO
+        return
+
+    def text_to_display(self, message):
+        # TODO
+        return
+
+    def clear_display(self):
+        # TODO
+        return
+
+    def setup_water_flow_sensor(self):
+        # TODO
+        return
+    
+    def speed_water_flow_sensor(self):
+        # TODO
+        return
+
+    def detect_water_flow_sensor(self):
+        # TODO
+        return
+
+    def setup_captouch(self):
+        # TODO
+        return
+
+    def detect_captouch(self):
+        # TODO
+        return
+
+    def setup_sound_system(self):
+        # TODO
+        return
+    
+    def play_sound(self, audio):
+        # TODO
+        return
+
+    def stop_sound(self):
+        # TODO
+        return
+
+    def setup_bubbles(self):
+        # TODO
+        return
+    
+    def start_bubbles(self):
+        # TODO
+        return
+
+    def stop_bubbles(self):
+        # TODO
+        return
+        
