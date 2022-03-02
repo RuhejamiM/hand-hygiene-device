c9613fcc7a0ad6d2f0ceacf580a1cc7dcbe76c3e
Ethan Bonpin <98905700+eb3326@users.noreply.github.com>
2022-03-02 18:48:49 -0500

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,2 +1,11 @@
-# TODO:
+from setuptools import setup
 
+setup(name='hand-hygiene',
+      version='1.0',
+      description='Library for Hand Hygiene Device for Children',
+      author='Ethan Bonpin and Ruhejami Mustari',
+      author_email=['eb3326@nyu.edu', 'rm4966@nyu.edu']
+      url='https://github.com/RuhejamiM/hand-hygiene-device',
+      install_requires=[''],
+	  py_modules=['hand-hygiene'],
+     )
