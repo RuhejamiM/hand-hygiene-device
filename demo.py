50f44884cd2d27eeeef0de7ba7889542d3ffedf9
Ethan Bonpin <eb3326@nyu.edu>
2022-04-06 13:25:28 -0400

    Update demo.py
    Added demos for Capacitive Touch Sensor, Bubble System, Water Flow Sensor

--- demo.py
+++ demo.py
@@ -37,7 +37,63 @@ Capacitive Touch Tests
 ct = CapTouch() # initialize capacitive touch object
 
 ct.setup_captouch()
+
+print("Now testing capacitive touch sensor input. Follow the prompts or press Ctrl + C to skip.")
+
 ct.detect_captouch()
 
+try:
+	while not GPIO.event_detected(ct.out0):
+		print("Touch the first capacitive input")
+	while not GPIO.event_detected(ct.out1):
+		print("Touch the second capacitive input")
+	while not GPIO.event_detected(ct.out2):
+		print("Touch the third/last capacitive input")
+	print("Success. All capacitive inputs are working.")
+except KeyboardInterrupt:
+	print("Ctrl + C was pressed. Terminating capacitive touch test and continuing...")
+
+"""
+Bubble System Tests
+"""
+
+b1 = Bubbles() # initialize bubble object
+
+b1.setup_bubbles()
+
+print("Now testing the bubble system. This will run the fan and start the servo for 10 seconds. Press Ctrl + C to skip.")
+
+try:
+	b1.start_bubbles()
+	time.sleep(10)
+	b1.stop_bubbles()
+except KeyboardInterrupt:
+	print("Ctrl + C was pressed. Terminating bubble system test and continuing...")
+
+"""
+Flow Sensor Tests
+"""
+
+fs = FlowSensor() # initialize flow sensor object
+
+fs.setup_water_flow_sensor()
+
+print("Now testing the flow sensor. This will start a counter from 0 and increase if water is flowing, else it will stay constant. Press Ctrl + C to skip.")
+time.sleep(3)
+
+flowcount = 0
+
+try:
+	flow_end = time.time() + 10
+	while time.time() < flow_end:
+		fc2 = flowcount
+		time.sleep(0.25)
+		if(flowcount == fc2):
+			print("No water flow detected.")
+		else:
+			print("Water flow detected.")
+		print("Current flow count: ", flowcount)
+except KeyboardInterrupt:
+	print("Ctrl + C was pressed. Terminating flow sensor test and continuing...")
 
 
